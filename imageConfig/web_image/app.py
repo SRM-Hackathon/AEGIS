4abffbdebb4d05f8b11308d3b649af26925f5629
ksmohamedimran <56427464+ksmohamedimran@users.noreply.github.com>
2019-10-12 12:26:32 +0530

    Update app.py

--- imageConfig/web_image/app.py
+++ imageConfig/web_image/app.py
@@ -1,57 +1,21 @@
+#Author Name - Imaya Bharathi
+#Date - 11-10-2019 -  12-10-2019
+#purpose - For Running Flask Server
+
 from flask import Flask,render_template,request,jsonify
 import urllib3
 urllib3.disable_warnings()
 
 from flask_content.vault_deploy import (create_user,on_page_load,authenticate_user_pass)
-# from vault_deploy import (create_user,on_page_load,authenticate_user_pass)
 app = Flask(__name__)
 count = 0
 @app.route("/")
 def index():
     return render_template("register.html")
-    # print("Getting vault credentials")
-    
-    # # vault_response = vault_deploy(  )
-    # if vault_response != 200 :
-    #     return "vault_doesn't recognize your key"
-    # else:
-    #     return "some prompt"
-  
-
-# @app.route("/register",methods=['POST'])
-# def register():
-#     print("from register *************************")
-#     username=request.form['reg_username']
-#     password=request.form['reg_password']
-#     print(username,password)
-#     # client_token = create_user(username,password)
-#     return render_template("register.html",client_token=jsonify(client_token))
-
-# @app.route('/process', methods=['POST'])
-# def process():
-# 	username = request.form['reg_username']
-# 	password = request.form['reg_password']
-#     # print(username)
-# 	if username and password:
-#         print(username)
-# 		newName = username[::-1]
-# 		return jsonify({'name' : newName})
-# 	return jsonify({'error' : 'Missing data!'})
-
-# @app.route('/process', methods=['POST'])
-# def process():
-#     username = request.form["nameInput"]
-#     password = request.form["passwordInput"]
-
-#     # password = request.form["passwordInput"]
-#     print(username)
-#     return jsonify({'error' : 'Missing data!'}) 
-    # password
 
 
 @app.route('/process', methods=['POST'])
 def process():
-    #it should be load only ones 
     if globals() ["count"] == 0:
         on_page_load()
         globals() ["count"] +=1
